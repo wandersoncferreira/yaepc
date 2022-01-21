3935fdd76125e7ea17f75b280dfbb2022ddd301b
Wanderson Ferreira <wand@hey.com>
2022-01-20 22:02:09 -0300

    add more states to org-todo

--- settings/setup-org.el
+++ settings/setup-org.el
@@ -2,7 +2,17 @@
 
 (setq org-directory "~/org"
       org-default-notes-file (concat org-directory "/notes.org")
-      org-return-follows-link t)
-
+      org-return-follows-link t
+      org-todo-keywords
+      '((sequence "TODO(t)"
+                  "IN-PROGRESS(p)"
+                  "|"
+                  "DONE(d)"
+                  "CANCELED(c)"
+                  "CANT-REPRODUCE(k)"))
+      org-todo-keyword-faces
+      '(("IN-PROGRESS" . "purple")
+        ("CANCELED" . org-warning)
+        ("CANT-REPRODUCE" . "red")))
 
 (provide 'setup-org)
