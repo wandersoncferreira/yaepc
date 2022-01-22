4067f501d854c7bd020d5511d3a5f109eb1ddd1d
Wanderson Ferreira <wand@hey.com>
2022-01-22 08:17:00 -0300

    new rss feeds

--- settings/setup-rss.el
+++ settings/setup-rss.el
@@ -1,7 +1,10 @@
 (require 'elfeed)
 
 (setq elfeed-feeds
-      '("https://karl-voit.at/feeds/lazyblorg-all.atom_1.0.links-and-content.xml"))
+      '("https://karl-voit.at/feeds/lazyblorg-all.atom_1.0.links-and-content.xml"
+        "https://emilkirkegaard.dk/en/feed"
+        "https://desmondrivet.com/feeds/blog.rss"
+        "https://xkcd.com/atom.xml"))
 
 (set-face-attribute 'elfeed-search-title-face nil :foreground "dim gray")
 (set-face-attribute 'elfeed-search-unread-title-face nil :foreground "powder blue")
