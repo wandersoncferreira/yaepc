ae373b2fbd89e09b5474714649b109b02bf66fa1
Wanderson Ferreira <wand@hey.com>
2022-01-22 17:04:13 -0300

    add new rss feed -> emacs redux

--- settings/setup-rss.el
+++ settings/setup-rss.el
@@ -4,7 +4,8 @@
       '("https://karl-voit.at/feeds/lazyblorg-all.atom_1.0.links-and-content.xml"
         "https://emilkirkegaard.dk/en/feed"
         "https://desmondrivet.com/feeds/blog.rss"
-        "https://xkcd.com/atom.xml"))
+        "https://xkcd.com/atom.xml"
+        "https://emacsredux.com/atom.xml"))
 
 (set-face-attribute 'elfeed-search-title-face nil :foreground "dim gray")
 (set-face-attribute 'elfeed-search-unread-title-face nil :foreground "powder blue")
