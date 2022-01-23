d5d818b4be273bf7ecfb7e26014ef558ff5d346a
Wanderson Ferreira <wand@hey.com>
2022-01-22 21:21:51 -0300

    dired use GNU ls

--- settings/setup-dired.el
+++ settings/setup-dired.el
@@ -4,4 +4,9 @@
 ;; Move files between split panes
 (setq dired-dwim-target t)
 
+;; use GNU ls
+(when is-mac
+  (setq insert-directory-program
+        (executable-find "gls")))
+
 (provide 'setup-dired)
