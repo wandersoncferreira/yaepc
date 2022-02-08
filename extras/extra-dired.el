0315697114c410d982fddf7b361eedb6c3a457f2
Wanderson Ferreira <wand@hey.com>
2022-02-07 23:53:37 -0300

    stop grouping directories in dired buffers

--- extras/extra-dired.el
+++ extras/extra-dired.el
@@ -4,9 +4,6 @@
 ;; Move files between split panes
 (setq dired-dwim-target t)
 
-;; require GNU ls to use group flag
-(setq dired-listing-switches "-al --group-directories-first")
-
 ;; use GNU ls
 (when is-mac
   (setq insert-directory-program
