2e27421d7018589e1d69dc2553978285dfacd2bc
Wanderson Ferreira <wand@hey.com>
2022-02-09 02:57:44 -0300

    new rss feed

--- extras/extra-rss.el
+++ extras/extra-rss.el
@@ -5,7 +5,8 @@
         "https://emilkirkegaard.dk/en/feed"
         "https://desmondrivet.com/feeds/blog.rss"
         "https://xkcd.com/atom.xml"
-        "https://emacsredux.com/atom.xml"))
+        "https://emacsredux.com/atom.xml"
+        "https://okmij.org/ftp/rss.xml"))
 
 (set-face-attribute 'elfeed-search-title-face nil :foreground "dim gray")
 (set-face-attribute 'elfeed-search-unread-title-face nil :foreground "powder blue")
