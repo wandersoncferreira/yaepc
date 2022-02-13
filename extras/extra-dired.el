0e217eca0fdc52ac96dd38892903cbfecaf25def
Wanderson Ferreira <wand@hey.com>
2022-02-12 22:15:07 -0300

    hide details in dired

--- extras/extra-dired.el
+++ extras/extra-dired.el
@@ -10,4 +10,13 @@
   (setq insert-directory-program
         (executable-find "gls")))
 
+;; make dired less verbose
+(add-hook 'dired-mode-hook (lambda () (dired-hide-details-mode +1)))
+
+;; bind dired delete on `k`
+(define-key dired-mode-map (kbd "k") 'dired-do-delete)
+
+;; enable 'a'-keybinding in dired - which opens the file and closes dired buffer
+(put 'dired-find-alternate-file 'disabled nil)
+
 (provide 'extra-dired)
